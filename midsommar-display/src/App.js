74fc16015adf3b7420fec5c415d53d334b061606
samWidlund <sam.wid415@gmail.com>
2025-06-16 08:43:31 +0200

    working inital react app

--- midsommar-display/src/App.js
+++ midsommar-display/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import midsummer from './img/midsummer.jpg';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="min-h-[200vh] relative">
+      <div 
+        className="fixed inset-0 w-full h-full bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${midsummer})` }}
+      />
+      
+      <div className="h-screen w-screen bg-black flex items-center justify-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-lg font-['Cormorant_Garamond'] tracking-wider">
+          midsommar hos Ines!
+        </h1>
+      </div>
+    </div>
   );
 }
 
